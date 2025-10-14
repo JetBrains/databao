f3e771f7d6d12bddbfb672805b41a433a7efb57d
rauf-kurbanov <kurbanov.re@gmail.com>
2025-10-14 21:34:57 +0200

    minor fix

--- portus/pipes/lazy.py
+++ portus/pipes/lazy.py
@@ -16,13 +16,13 @@ class LazyPipe(Pipe):
         self.__session = session
         self.__default_rows_limit = default_rows_limit
 
-        self._data_materialized = False
-        self._data_materialized_rows: int | None = None
-        self._data_result: ExecutionResult | None = None
-        self._visualization_materialized = False
-        self._visualization_result: VisualisationResult | None = None
-        self._opas: list[Opa] = []
-        self._meta: dict[str, Any] = {}
+        self.__data_materialized = False
+        self.__data_materialized_rows: int | None = None
+        self.__data_result: ExecutionResult | None = None
+        self.__visualization_materialized = False
+        self.__visualization_result: VisualisationResult | None = None
+        self.__opas: list[Opa] = []
+        self.__meta: dict[str, Any] = {}
 
     def __materialize_data(self, rows_limit: int | None) -> "ExecutionResult":
         rows_limit = rows_limit if rows_limit else self.__default_rows_limit
@@ -30,50 +30,50 @@ class LazyPipe(Pipe):
             self.__data_result = self.__session.executor.execute(
                 self.__session, self.__opas, rows_limit=rows_limit, cache_scope=str(id(self))
             )
-            self._data_materialized = True
-            self._data_materialized_rows = rows_limit
-            self._meta.update(self._data_result.meta)
-        if self._data_result is None:
+            self.__data_materialized = True
+            self.__data_materialized_rows = rows_limit
+            self.__meta.update(self.__data_result.meta)
+        if self.__data_result is None:
             raise RuntimeError("__data_result is None after materialization")
-        return self._data_result
+        return self.__data_result
 
     def __materialize_visualization(self, request: str, rows_limit: int | None) -> "VisualisationResult":
         self.__materialize_data(rows_limit)
-        if self._data_result is None:
+        if self.__data_result is None:
             raise RuntimeError("__data_result is None after materialization")
-        if not self._visualization_materialized:
-            self._visualization_result = self._session.visualizer.visualize(
-                request, self._session.llm, self._data_result
+        if not self.__visualization_materialized:
+            self.__visualization_result = self.__session.visualizer.visualize(
+                request, self.__session.llm, self.__data_result
             )
-            self._visualization_materialized = True
-            self._meta.update(self._visualization_result.meta)
-            self._meta["plot_code"] = self._visualization_result.code  # maybe worth to expand as a property later
-        if self._visualization_result is None:
+            self.__visualization_materialized = True
+            self.__meta.update(self.__visualization_result.meta)
+            self.__meta["plot_code"] = self.__visualization_result.code  # maybe worth to expand as a property later
+        if self.__visualization_result is None:
             raise RuntimeError("__visualization_result is None after materialization")
-        return self._visualization_result
+        return self.__visualization_result
 
     def df(self, *, rows_limit: int | None = None) -> DataFrame | None:
-        return self.__materialize_data(rows_limit if rows_limit else self._data_materialized_rows).df
+        return self.__materialize_data(rows_limit if rows_limit else self.__data_materialized_rows).df
 
     def plot(self, request: str = "visualize data", *, rows_limit: int | None = None) -> Any | None:
         return self.__materialize_visualization(
-            request, rows_limit if rows_limit else self._data_materialized_rows
+            request, rows_limit if rows_limit else self.__data_materialized_rows
         ).plot
 
     def text(self) -> str:
-        return self.__materialize_data(self._data_materialized_rows).text
+        return self.__materialize_data(self.__data_materialized_rows).text
 
     def __str__(self) -> str:
         return self.text()
 
     def ask(self, query: str) -> Pipe:
-        self._opas.append(Opa(query=query))
+        self.__opas.append(Opa(query=query))
         return self
 
     @property
     def meta(self) -> dict[str, Any]:
-        return self._meta
+        return self.__meta
 
     @property
     def code(self) -> str | None:
-        return self.__materialize_data(self._data_materialized_rows).code
+        return self.__materialize_data(self.__data_materialized_rows).code
