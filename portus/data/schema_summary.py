0d8c96d6e6dfd7fb75abe33eb7b6c1acba9cc830
Marko Hostnik <marko.hostnik@jetbrains.com>
2025-10-24 16:59:56 +0200

    Shorten column_value_stats_summary.jinja text descriptions

--- portus/data/schema_summary.py
+++ portus/data/schema_summary.py
@@ -19,13 +19,16 @@ def summarize_database_metadata(schema: DatabaseSchema, *, include_name: bool = 
 
 def summarize_table_schema(table_schema: TableSchema) -> str:
     # TODO make it configurable whether to use fully qualified names or not
+    # TODO general table stats: num rows, num columns
     s = f"\n## Table `{table_schema.qualified_name}`\n"
     if table_schema.description:
         s += f"{table_schema.description}\n"
     for column_name, column_schema in table_schema.columns.items():
         desc = "" if not column_schema.description else f": {column_schema.description}."
         if not column_schema.value_stats.is_empty():
-            desc = f"{desc} {column_schema.value_stats.summarize()}"
+            value_stats_summary = column_schema.value_stats.summarize()
+            value_stats_summary = value_stats_summary.strip()
+            desc = f"{desc} {value_stats_summary}"
         if column_schema.values:
             if desc:
                 desc = f"{desc} Values: {', '.join(column_schema.values)}"
