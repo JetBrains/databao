9978191de23646e537c3861294526b356e5e00be
Anna Rodionova <Anna.Rodionova@jetbrains.com>
2026-01-27 12:20:08 +0100

    [DBA-49] DCE Integration - Initial Version

--- databao/api.py
+++ databao/api.py
@@ -1,7 +1,9 @@
 from databao.caches.in_mem_cache import InMemCache
 from databao.configs.agent import DEFAULT_AGENT_CONFIG, AgentConfig
 from databao.configs.llm import LLMConfig, LLMConfigDirectory
-from databao.core import Agent, Cache, Executor, Visualizer
+from databao.core import AgentV1, Cache, Executor, Visualizer
+from databao.core.v2.agent import AgentV2
+from databao.core.v2.context import Context
 from databao.executors.lighthouse.executor import LighthouseExecutor
 from databao.visualizers.vega_chat import VegaChatVisualizer
 
@@ -18,13 +20,48 @@ def new_agent(
     stream_plot: bool = False,
     lazy_threads: bool = False,
     auto_output_modality: bool = True,
-) -> Agent:
+) -> AgentV1:
     """This is an entry point for users to create a new agent.
     Agent can't be modified after it's created. Only new data sources can be added.
     """
     llm_config = llm_config if llm_config else LLMConfigDirectory.DEFAULT
     agent_config = agent_config if agent_config else DEFAULT_AGENT_CONFIG
-    return Agent(
+    return AgentV1(
+        llm_config,
+        agent_config,
+        name=name or "default_agent",
+        data_executor=data_executor or LighthouseExecutor(),
+        visualizer=visualizer or VegaChatVisualizer(llm_config),
+        cache=cache or InMemCache(),
+        rows_limit=rows_limit,
+        stream_ask=stream_ask,
+        stream_plot=stream_plot,
+        lazy_threads=lazy_threads,
+        auto_output_modality=auto_output_modality,
+    )
+
+
+def new_agent_v2(
+    context: Context,
+    name: str | None = None,
+    llm_config: LLMConfig | None = None,
+    agent_config: AgentConfig | None = None,
+    data_executor: Executor | None = None,
+    visualizer: Visualizer | None = None,
+    cache: Cache | None = None,
+    rows_limit: int = 1000,
+    stream_ask: bool = True,
+    stream_plot: bool = False,
+    lazy_threads: bool = False,
+    auto_output_modality: bool = True,
+) -> AgentV2:
+    """This is an entry point for users to create a new agent.
+    Agent can't be modified after it's created. Only new data sources can be added.
+    """
+    llm_config = llm_config if llm_config else LLMConfigDirectory.DEFAULT
+    agent_config = agent_config if agent_config else DEFAULT_AGENT_CONFIG
+    return AgentV2(
+        context,
         llm_config,
         agent_config,
         name=name or "default_agent",
