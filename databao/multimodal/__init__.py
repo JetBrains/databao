104bfb84962afc5bc529f6a2f921def242ba58c0
Viacheslav Ermakov <viacheslav.ermakov@jetbrains.com>
2026-02-05 10:51:56 +0100

    Add multimodal-jupyter app with Jupyter widget integration

--- databao/multimodal/__init__.py
+++ databao/multimodal/__init__.py
@@ -1,7 +1,10 @@
 """Databao viewer module for displaying multimodal tabs in the browser."""
 
 from databao.multimodal.html_viewer import open_html_content
+from databao.multimodal.jupyter_widget import MultimodalWidget, create_jupyter_widget
 
 __all__ = [
+    "MultimodalWidget",
+    "create_jupyter_widget",
     "open_html_content",
 ]
