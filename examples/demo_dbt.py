e2e7b55346316bd3be9b5480fba8c43b40d77d36
Andrei Gasparian <andrei.gasparian@jetbrains.com>
2026-02-11 12:52:40 +0100

    drop dbt v2 implenentation for now

--- examples/demo_dbt.py
+++ examples/demo_dbt.py
@@ -1,67 +1,50 @@
 import logging
-logging.basicConfig(level=logging.DEBUG)
+from pathlib import Path
 
 import duckdb
 import databao
 from databao import LLMConfig
+from databao.configs.agent import AgentConfig
+from databao.executors.dbt import DbtConfig, DbtProjectExecutor
 
-from dbt_agent_tmp.langchain_agent import LangchainAgent
+logging.basicConfig(level=logging.INFO)
 
-
-# partially completed dbt proj (copy from the original Spider2-dbt dataset)
-DB_PATH = "/Users/andrei.gasparian/Documents/spider-2.0/Spider2/spider2-dbt/examples/shopify001/shopify.duckdb"
-DBT_PROJ_PATH = "/Users/andrei.gasparian/Documents/databao-agent/examples/shopify001"
-
-conn = duckdb.connect(DB_PATH)
+DB_PATH = "/Users/andrei.gasparian/Documents/databao-agent/examples/shopify002/shopify.duckdb"
+DBT_PROJ_PATH = Path("/Users/andrei.gasparian/Documents/databao-agent/examples/shopify002")
 
 llm_config = LLMConfig(name="gpt-5", temperature=0)
-agent = databao.new_agent(name="demo-dbt", llm_config=llm_config)
+agent_config = AgentConfig(recursion_limit=100, parallel_tool_calls=True)
 
-agent.add_db(conn)
-thread = agent.thread()
-
-df = thread.ask("generate customers cohorts").df()
-print(df.head())
-print()
-print("--------")
-print()
-print(thread.text())
-
-"""
-use-case: 
-  - user can ask agent so generated metric could be better fit to the real life
-  - maybe user fixes could be additionally trigger "memorize" or some other new method so dbt agent could use it
-  - them summarize the hwole thing (history + user hints)
-  - then dbt agent kicks in, uses that summary + memorized hints (?)
-  - add ability to materialize dbt models separately: .confirm()? or .materialize()?
-  
-technical:
-  - 
-"""
-
-# TODO: (@gas) could summary be replaced with thread.text() + thread.code()?
-thread.ask("summarize you solution into the brief description of what's has been done supplying it with the final set of sql queries, needed to reproduce the result in the future")
-print()
-print("--------")
-print()
-print(thread.text())
-
-
-task_prompt = "Given the following summary, add missing data models:\n" + thread.text()
-
-dbt_agent = LangchainAgent(
-    name="langchain_dbt_agent",
-    project_dir=DBT_PROJ_PATH,
-    system_prompt_name="system_prompt.jinja",
-    db_conn=conn,
+agent = databao.new_agent(
+    name="demo-dbt-executor",
+    llm_config=llm_config,
+    agent_config=agent_config,
+    data_executor=DbtProjectExecutor(
+        dbt_config=DbtConfig(
+            project_dir=DBT_PROJ_PATH,
+        ),
+        use_sandbox=True,
+    ),
 )
-output = dbt_agent.run(task_prompt)
-print()
-print("--------")
-print()
-print(output)
 
-# ---
-# # Generate a visualization (Vega-Lite under the hood)
-# plot = thread.plot("bar chart of shows by country")
-# print(plot.code)  # access generated plot code if needed
+conn = duckdb.connect(DB_PATH)
+agent.add_db(conn)
+
+thread = agent.thread(stream_ask=True)
+
+# "What is our 90-day repeat purchase rate"
+# "What share of orders use a discount code (Discount attach rate)"
+# "What is our abandoned checkout recovery rate within 7 days"
+# "How long does it take to fulfill an order?"
+thread.ask(
+    "What is our refund rate by month?"
+)
+
+print("\n=== TEXT ===\n")
+print(thread.text())
+
+print("\n=== CODE ===\n")
+print(thread.code())
+
+print("\n=== Dataframe ===\n")
+print(thread.df())
