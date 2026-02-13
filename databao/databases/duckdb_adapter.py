417bd6cb0d3b151c9a7244e32852fd5080e93a39
Yury Khudyakov <yury.khudyakov@jetbrains.com>
2026-02-13 13:28:41 +0100

    [#159] Revert change for duckdb_adapter

--- databao/databases/duckdb_adapter.py
+++ databao/databases/duckdb_adapter.py
@@ -42,7 +42,7 @@ class DuckDBAdapter(DatabaseAdapter):
 
     @classmethod
     def register_in_duckdb(cls, shared_conn: DuckDBPyConnection, config: DBConnectionConfig, name: str) -> None:
-        database = config.content.get("connection", {}).get(DATABASE_KEY)
+        database = config.content.get(DATABASE_KEY)
         shared_conn.execute(f"ATTACH '{database}' AS \"{name}\" (READ_ONLY);")
 
     @staticmethod
