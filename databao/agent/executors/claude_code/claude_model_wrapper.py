1149f1d2f11b1959c2c0a815133fd5857021018d
pickleerik <erik.koynov@jetbrains.com>
2026-03-10 18:29:51 +0200

    refactor logging into a single function

--- databao/agent/executors/claude_code/claude_model_wrapper.py
+++ databao/agent/executors/claude_code/claude_model_wrapper.py
@@ -10,11 +10,12 @@ from typing import Any, TextIO
 
 import pandas as pd
 from _duckdb import DuckDBPyConnection
-from claude_agent_sdk import ClaudeAgentOptions, ClaudeSDKClient, SdkMcpTool, UserMessage, create_sdk_mcp_server, tool
+from claude_agent_sdk import ClaudeAgentOptions, ClaudeSDKClient, SdkMcpTool, UserMessage, create_sdk_mcp_server, tool, \
+    AssistantMessage
 from claude_agent_sdk.types import McpSdkServerConfig, ToolResultBlock
 from claude_agent_sdk.types import Message as ClaudeMessage
 from claude_agent_sdk.types import SystemMessage as ClaudeSystemMessage
-from langchain_core.messages import AIMessage, AIMessageChunk, BaseMessage
+from langchain_core.messages import AIMessage, AIMessageChunk, BaseMessage, ToolMessage
 
 from databao.agent.configs.llm import LLMConfig
 from databao.agent.core.executor import ExecutionResult
@@ -220,13 +221,13 @@ class ClaudeModelWrapper:
         sql_history: list[str] = []
         for message in self.solve(prompt):
             langchain_message = cast_claude_message_to_langchain_message(message)
-            if isinstance(langchain_message, AIMessage) and stream:
-                frontend.write_stream_chunk("messages", (AIMessageChunk(content=langchain_message.content), {}))
 
             if isinstance(langchain_message, list):
                 message_log.extend(langchain_message)
             else:
                 message_log.append(langchain_message)
+
+            _log_message(langchain_message, frontend, stream=stream)
 
             if not isinstance(message, UserMessage):
                 continue
@@ -246,6 +247,15 @@ class ClaudeModelWrapper:
             df=df_history[-1] if df_history else None,
         )
 
+def _log_message(message: BaseMessage | list[BaseMessage], frontend: TextStreamFrontend, stream: bool = False) -> None:
+    if not stream:
+        return
+    if isinstance(message, AIMessage):
+        frontend.write_stream_chunk("messages", (AIMessageChunk(content=message.content), {}))
+    elif isinstance(message, ToolMessage):
+        frontend.write_stream_chunk("values", {"messages": [message]})
+    elif isinstance(message, list):
+        frontend.write_stream_chunk("values", {"messages": message})
 
 def _extract_sql_and_dataframe(message: UserMessage) -> tuple[str | None, pd.DataFrame | None]:
     for result_block in message.content:
